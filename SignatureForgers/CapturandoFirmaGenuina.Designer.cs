43b60e96f768d9c7786766733b0633fc3e3abea0
Laura B <laubenmar@gmail.com>
2015-03-10 16:47:13 +0100

    Primer intento añadir código de la Wacom
    De momento da error aunque haya copiado el código (añadiendo o
    renombrando las variables necesarias y eliminando lo no necesario)

--- SignatureForgers/CapturandoFirmaGenuina.Designer.cs
+++ SignatureForgers/CapturandoFirmaGenuina.Designer.cs
@@ -28,16 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.signControlWacomSTU1 = new SignCaptureWacomSTU.SignControlWacomSTU();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.bClearWacom = new System.Windows.Forms.Button();
+            this.bStopWacom = new System.Windows.Forms.Button();
+            this.bStartWacom = new System.Windows.Forms.Button();
+            this.bSetImage = new System.Windows.Forms.Button();
+            this.bBrowse = new System.Windows.Forms.Button();
+            this.bSaveSignature = new System.Windows.Forms.Button();
+            this.labelSamples = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // signControlWacomSTU1
+            // 
+            this.signControlWacomSTU1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.signControlWacomSTU1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.signControlWacomSTU1.Location = new System.Drawing.Point(2, 1);
+            this.signControlWacomSTU1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.signControlWacomSTU1.Name = "signControlWacomSTU1";
+            this.signControlWacomSTU1.Size = new System.Drawing.Size(384, 288);
+            this.signControlWacomSTU1.StrokeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(221)))));
+            this.signControlWacomSTU1.StrokeMaxThickPressure = 3.5F;
+            this.signControlWacomSTU1.StrokeThickNoVisiblePressure = 3F;
+            this.signControlWacomSTU1.StrokeThickNoVisiblePressureToExport = 3F;
+            this.signControlWacomSTU1.StrokeVisiblePressure = false;
+            this.signControlWacomSTU1.TabIndex = 0;
+            this.signControlWacomSTU1.WacomSignArea = null;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 62);
+            this.label1.Location = new System.Drawing.Point(37, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(362, 24);
             this.label1.TabIndex = 0;
@@ -47,7 +72,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(38, 144);
+            this.label2.Location = new System.Drawing.Point(12, 84);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(190, 16);
             this.label2.TabIndex = 1;
@@ -56,17 +81,93 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(234, 147);
+            this.label3.Location = new System.Drawing.Point(199, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(13, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "5";
+            // 
+            // bClearWacom
+            // 
+            this.bClearWacom.Location = new System.Drawing.Point(330, 135);
+            this.bClearWacom.Name = "bClearWacom";
+            this.bClearWacom.Size = new System.Drawing.Size(100, 32);
+            this.bClearWacom.TabIndex = 3;
+            this.bClearWacom.Text = "Clear";
+            this.bClearWacom.UseVisualStyleBackColor = true;
+            this.bClearWacom.Click += new System.EventHandler(this.bClearWacom_Click);
+            // 
+            // bStopWacom
+            // 
+            this.bStopWacom.Location = new System.Drawing.Point(185, 135);
+            this.bStopWacom.Name = "bStopWacom";
+            this.bStopWacom.Size = new System.Drawing.Size(101, 32);
+            this.bStopWacom.TabIndex = 4;
+            this.bStopWacom.Text = "Stop";
+            this.bStopWacom.UseVisualStyleBackColor = true;
+            this.bStopWacom.Click += new System.EventHandler(this.bStopWacom_Click);
+            // 
+            // bStartWacom
+            // 
+            this.bStartWacom.Location = new System.Drawing.Point(41, 135);
+            this.bStartWacom.Name = "bStartWacom";
+            this.bStartWacom.Size = new System.Drawing.Size(101, 32);
+            this.bStartWacom.TabIndex = 5;
+            this.bStartWacom.Text = "Start";
+            this.bStartWacom.UseVisualStyleBackColor = true;
+            this.bStartWacom.Click += new System.EventHandler(this.bStartWacom_Click);
+            // 
+            // bSetImage
+            // 
+            this.bSetImage.Location = new System.Drawing.Point(41, 177);
+            this.bSetImage.Name = "bSetImage";
+            this.bSetImage.Size = new System.Drawing.Size(101, 32);
+            this.bSetImage.TabIndex = 8;
+            this.bSetImage.Text = "Set Image";
+            this.bSetImage.UseVisualStyleBackColor = true;
+            this.bSetImage.Click += new System.EventHandler(this.bSetImage_Click);
+            // 
+            // bBrowse
+            // 
+            this.bBrowse.Location = new System.Drawing.Point(185, 177);
+            this.bBrowse.Name = "bBrowse";
+            this.bBrowse.Size = new System.Drawing.Size(101, 32);
+            this.bBrowse.TabIndex = 7;
+            this.bBrowse.Text = "Browse";
+            this.bBrowse.UseVisualStyleBackColor = true;
+            this.bBrowse.Click += new System.EventHandler(this.bBrowse_Click);
+            // 
+            // bSaveSignature
+            // 
+            this.bSaveSignature.Location = new System.Drawing.Point(330, 177);
+            this.bSaveSignature.Name = "bSaveSignature";
+            this.bSaveSignature.Size = new System.Drawing.Size(100, 32);
+            this.bSaveSignature.TabIndex = 6;
+            this.bSaveSignature.Text = "Save Signature";
+            this.bSaveSignature.UseVisualStyleBackColor = true;
+            this.bSaveSignature.Click += new System.EventHandler(this.bSaveSignature_Click);
+            // 
+            // labelSamples
+            // 
+            this.labelSamples.AutoSize = true;
+            this.labelSamples.Location = new System.Drawing.Point(327, 84);
+            this.labelSamples.Name = "labelSamples";
+            this.labelSamples.Size = new System.Drawing.Size(65, 13);
+            this.labelSamples.TabIndex = 9;
+            this.labelSamples.Text = "Muestra 1/5";
             // 
             // CapturandoFirmaGenuina
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 221);
+            this.Controls.Add(this.labelSamples);
+            this.Controls.Add(this.bSetImage);
+            this.Controls.Add(this.bBrowse);
+            this.Controls.Add(this.bSaveSignature);
+            this.Controls.Add(this.bStartWacom);
+            this.Controls.Add(this.bStopWacom);
+            this.Controls.Add(this.bClearWacom);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -82,9 +183,19 @@
         }
 
         #endregion
+        
+        private SignCaptureWacomSTU.SignControlWacomSTU signControlWacomSTU1;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bClearWacom;
+        private System.Windows.Forms.Button bStopWacom;
+        private System.Windows.Forms.Button bStartWacom;
+        private System.Windows.Forms.Button bSetImage;
+        private System.Windows.Forms.Button bBrowse;
+        private System.Windows.Forms.Button bSaveSignature;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label labelSamples;
     }
 }
--- SignatureForgers/CapturandoFirmaGenuina.cs
+++ SignatureForgers/CapturandoFirmaGenuina.cs
@@ -7,32 +7,287 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SignCaptureWacomSTU;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
 
 namespace SignatureForgers
 {
+    public delegate void UpdateLabelSamples();
+
     public partial class CapturandoFirmaGenuina : Form
     {        
-        int numberOfGenuineSignatures;
+        /***/
+        private WacomButton buttonClearSignature;
+        private WacomButton buttonSaveSignature;
+        private int currentSample = 1;
+        private readonly string folderSave = Application.StartupPath;        
+        /***/     
+        int numberOfGenuineSignatures = 3;
 
         public CapturandoFirmaGenuina()
         {
             
             InitializeComponent();
             changeLabelName();
+
+            /***/
+            saveFileDialog1.InitialDirectory = folderSave;
+            signControlWacomSTU1.TabletaNoConectada += signControlWacomSTU1_TabletaNoConectada;
+            /***/
         }
         private void changeLabelName()
         {
             //TO DO Que el numero de muestras venga de la configuración.
 
             string numberToShowInLabel;
-
-            numberOfGenuineSignatures = 5;
-
+                        
             numberToShowInLabel = Convert.ToString(numberOfGenuineSignatures);
 
             label3.Text = numberToShowInLabel;
          
 
         }
+
+
+        /***************************************************************************/
+        /***                  Metodos del otro proyecto                         ***/
+
+        #region Event Handlers
+
+        private void bStartWacom_Click(object sender, EventArgs e)
+        {           
+
+            bStartWacom.Enabled = false;
+
+            signControlWacomSTU1.WacomButtons = new WacomButtons();
+
+            buttonClearSignature = new WacomButton
+            {
+                PointTopLeft = new Point(495, 24),
+                PointBottomRight = new Point(623, 75),
+                Type = WacomButtonType.Clear,
+                Name = @"Borrar",
+                Enabled = true,
+            };
+            signControlWacomSTU1.WacomButtons.Add(buttonClearSignature);
+
+            buttonSaveSignature = new WacomButton
+            {
+                PointTopLeft = new Point(336, 24),
+                PointBottomRight = new Point(464, 75),
+                Type = WacomButtonType.SaveSignature,
+                Name = @"Guardar",
+                Enabled = true,
+            };
+            signControlWacomSTU1.WacomButtons.Add(buttonSaveSignature);
+
+            foreach (var button in signControlWacomSTU1.WacomButtons)
+            {
+                button.ButtonPressed += buttonWacom_ButtonPressed;
+            }
+
+            //Parámetros gráficos de generación de imagen de firma (Ajustados para Wacom STU-500)
+            signControlWacomSTU1.StrokeThickNoVisiblePressure = 1.6f;
+            signControlWacomSTU1.StrokeThickNoVisiblePressureToExport = 3f;
+            signControlWacomSTU1.StrokeMaxThickPressure = 3.5f;
+            signControlWacomSTU1.StrokeVisiblePressure = true;
+            signControlWacomSTU1.StrokeColor = Color.Black;
+
+            signControlWacomSTU1.start();
+
+            if (signControlWacomSTU1.IsWacomConnected)
+            {
+                /* [Laura] He cambiado el segundo parametro de GetBitmap con el numero
+                 * de muestras como constante, no con la selección del nudSamples
+                 * 
+                 */
+                signControlWacomSTU1.SetImage(GetBitmap(1, numberOfGenuineSignatures, true));
+            }
+            else
+            {
+                MessageBox.Show("ErrorInitializingWacom");
+            }
+
+            bStartWacom.Enabled = true;
+        }
+
+        private void bStopWacom_Click(object sender, EventArgs e)
+        {
+            bStopWacom.Enabled = false;
+
+            signControlWacomSTU1.Stop();
+
+            bStopWacom.Enabled = true;
+        }
+
+        private void bClearWacom_Click(object sender, EventArgs e)
+        {
+            signControlWacomSTU1.cleanSignature(true);
+        }
+
+        private void bSetImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog browseFile = new OpenFileDialog();
+            browseFile.InitialDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"");
+            browseFile.Filter = "Image Files (*.bmp, *.png)| *.bmp; *.png";
+            browseFile.Title = "Seleccione archivo de imagen";
+            if (browseFile.ShowDialog() == DialogResult.OK)
+            {
+                string imageLocation = browseFile.FileName;
+                signControlWacomSTU1.SetImage(new Bitmap(imageLocation));
+            }
+
+        }
+
+        private void bBrowse_Click(object sender, EventArgs e)
+        {
+            string folder = string.IsNullOrEmpty(saveFileDialog1.InitialDirectory)
+                          ? folderSave
+                          : saveFileDialog1.InitialDirectory;
+            Process.Start("explorer.exe", folder);
+        }
+
+        private void bSaveSignature_Click(object sender, EventArgs e)
+        {
+            byte[] birBytes;
+            byte[] imageBytes;
+            bool signedOutsideLimits;
+            bool ret = signControlWacomSTU1.getSignature(out birBytes, out imageBytes, out signedOutsideLimits);
+
+            if (ret)
+            {
+                /*
+                 * [Laura] Ahora guarda siempre los archivos tanto en bir como en imagen
+                 * Antes había un check que permitía elegir en cual/es querías
+                 */
+                SaveFiles(birBytes, imageBytes);
+            }
+        }
+
+        #endregion
+                
+
+        #region Wacom Event Handlers
+
+        private void buttonWacom_ButtonPressed(object sender, WacomButtonPressedEventArgs e)
+        {
+            if (sender != null)
+            {
+                WacomButtonType type = ((WacomButton)sender).Type;
+
+                switch (type)
+                {
+                    case WacomButtonType.Clear:
+
+                        signControlWacomSTU1.cleanSignature(true);
+                        break;
+
+                    case WacomButtonType.SaveSignature:
+
+                        byte[] birBytes = e.WacomButtonPressedEventArgsParams.BirBytes;
+                        byte[] imageBytes = e.WacomButtonPressedEventArgsParams.ImageBytes;
+
+                        /*
+                         * [Laura] Ahora guarda siempre los archivos tanto en bir como en imagen
+                         * Antes había un check que permitía elegir en cual/es querías
+                         */
+                        SaveFiles(birBytes, imageBytes);
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void signControlWacomSTU1_TabletaNoConectada(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"Tableta no conectada");
+        }
+
+        #endregion
+
+        #region Private Methods
+
+                /*
+                 * TO DO -- Cambiar el formato en el que guarda los archivos
+                 * Y hacer que lo guarde en la carpeta correspondiente
+                 */
+        private void SaveFiles(byte[] birBytes, byte[] imageBytes)
+        {
+            DateTime now = DateTime.Now;
+
+            if (birBytes != null)
+            {
+                string fileNameBir = string.Format("{0}{1}{2}{3}-{4}{5}{6}-{7}.bir",
+                                                folderSave.EndsWith("\\") || folderSave.EndsWith("/") ? folderSave : folderSave + Path.DirectorySeparatorChar,
+                                                now.Year,
+                                                now.Month.ToString("D2"),
+                                                now.Day.ToString("D2"),
+                                                now.Hour.ToString("D2"),
+                                                now.Minute.ToString("D2"),
+                                                now.Second.ToString("D2"),
+                                                now.Millisecond.ToString("D3"));
+                File.WriteAllBytes(fileNameBir, birBytes);
+            }
+
+            if (imageBytes != null)
+            {
+                string fileNameImage = string.Format("{0}{1}{2}{3}-{4}{5}{6}-{7}.png",
+                                                folderSave.EndsWith("\\") || folderSave.EndsWith("/") ? folderSave : folderSave + Path.DirectorySeparatorChar,
+                                                now.Year,
+                                                now.Month.ToString("D2"),
+                                                now.Day.ToString("D2"),
+                                                now.Hour.ToString("D2"),
+                                                now.Minute.ToString("D2"),
+                                                now.Second.ToString("D2"),
+                                                now.Millisecond.ToString("D3"));
+                File.WriteAllBytes(fileNameImage, imageBytes);
+            }
+
+            currentSample++;
+            if (currentSample > numberOfGenuineSignatures)
+            {
+                currentSample = 1;
+            }
+
+            signControlWacomSTU1.cleanSignature(false);
+            signControlWacomSTU1.SetImage(GetBitmap(currentSample, numberOfGenuineSignatures, true));
+
+            Invoke(new UpdateLabelSamples(UpdateLabelSamples));
+        }
+
+        private Bitmap GetBitmap(int sampleCurrent, int? sampleTotal, bool showSamples)
+        {
+            Bitmap bitmap = new Bitmap(Properties.Resources.PantallaFirma11);
+
+            if (showSamples)
+            {
+                string samplesText = sampleTotal == null
+                    ? string.Format(@"Muestra: {0}", sampleCurrent)
+                    : string.Format(@"Muestra: {0}/{1}", sampleCurrent, sampleTotal);
+
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.DrawString(samplesText,
+                                    new Font(FontFamily.GenericSansSerif, 20.0F, FontStyle.Bold),
+                                    new SolidBrush(Color.Black),
+                                    new PointF(38, 28));
+            }
+
+            return bitmap;
+        }
+
+        private void UpdateLabelSamples()
+        {
+            labelSamples.Text = string.Format(@"Muestra {0}/{1}", currentSample, numberOfGenuineSignatures);
+        }
+
+        #endregion
+
+      
     }
 }
